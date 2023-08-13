7010656071d62f1f58d2f17fafd7c2cb4b3a3ba3
Noel Emmanuel <emmanwa000@gmail.com>
2023-08-12 20:54:35 -0700

    Fixed code failure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [x in my_string if x != 'C' and x != 'c']
+    new_string = [x for x in my_string if x != 'C' and x != 'c']
     return ("".join(new_string))
