f55acf5acfdcdc35b106f83ebf9dba8de6f681ed
Noel Emmanuel <emmanwa000@gmail.com>
2023-08-28 13:48:53 -0700

    Fixed code failure

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    index = 0 
-    try:
-        while count < x:
-            try:
+
+    index = int_counts = 0
+    while True:
+        try:
+            if index < x:
                 print("{:d}".format(my_list[index]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                pass
+                index += 1
+                int_counts += 1
+            else:
+                print()
+                return int_counts
+        except (ValueError, TypeError):
             index += 1
-    except IndexError:
-        raise
-    finally:
-        print("")
-    return count
