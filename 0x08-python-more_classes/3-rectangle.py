c939f6a69469988dce9b01219e46517949eda14d
Noel Emmanuel <emmanwa000@gmail.com>
2023-09-04 13:29:14 -0700

    Fixed code failure(1)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -72,12 +72,15 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle.
-
-        Returns:
-            str: A string containing the rectangle represented by '#'
-            characters.
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join(['#' * self.__width for _ in range(self.__height)])      
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
